9ea9b758b77a03295583a6b439f1b4faba5a3eb2
Shougotmollik <Shougotmollik@gmail.com>
2023-07-22 19:30:16 +0600

    adding operator

---
+++ operators/RelationalOperator.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        /*
+         Relational Operator 
+
+         == (Equal to)
+
+         != ( Not Equla to)
+         > ( greater than)
+         < ( less than)
+         >= ( greater than equal)
+         <= ( less than equal)
+
+         */
+    }
+}
