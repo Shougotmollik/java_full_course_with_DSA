7c88805d73b5343676032163de67f47e876d9386
Shougotmollik <Shougotmollik@gmail.com>
2023-07-27 19:57:22 +0600

    adding Hollow Rectangle

---
+++ Patterns (part 2)/HollowRectangle.java
@@ -0,0 +1,23 @@
+public class HollowRectangle {
+
+    //crreating a function 
+    public static void hollow_rectangle(int totRows, int totCols){
+        // creating a outter loop
+        for (int i = 1; i <=totRows; i++) {
+            // creating a inner loop ==> columns
+            for(int j=1;j<=totCols;j++){  // cell ==> (i,j)
+                if(i==1||i==totRows||j==1||j==totCols){
+                    //boundary cell 
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println( );
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_rectangle(4, 5);
+    }
+}
