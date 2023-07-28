e8350c8dd26f9c465215cf2331f885304e9a98c0
Shougotmollik <Shougotmollik@gmail.com>
2023-07-28 21:38:21 +0600

    Adding arrays

---
+++ Arrays/ArrayCreation.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class ArrayCreation {
+    public static void main(String[] args) {
+        /*
+         array creation 
+
+         ==> datatype arrayname []=new datatype [size]
+         */
+
+        //creating integer array 
+        int marks[]=new int [50];
+        int number[]={50,70,74,58};
+
+        System.out.println(marks);
+        System.out.println(number);
+
+        //creating string array 
+
+        String sports[]={"Football","Cricket","Basketball","Hokey"};
+        System.out.println(sports);
+    }
+}
