ab535098ff88e6a71c17f1e35bbacbb144c3d504
Shougotmollik <Shougotmollik@gmail.com>
2023-08-01 20:42:40 +0600

    adding linear search algorithm

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,7 +1,24 @@
 package Arrays;
 
 public class LinearSearch {
+    //creating a function 
+    public static int leSrch(int numbers[],int key){
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i]==key) {
+                return i;
+            }
+        }
+         return -1;
+    }
     public static void main(String[] args) {
-        
+        int numbers[]={23,45,56,67,66,85,78,78,89};
+        int key=66;
+
+        int index=leSrch(numbers, key);
+        if(index==-1){
+            System.out.println("Print NOT found");
+        }else{
+            System.out.println("The index is "+index);
+        }
     }
 }
