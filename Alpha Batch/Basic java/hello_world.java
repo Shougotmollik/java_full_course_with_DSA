f46c5d6bed805d5387044bb62160fd8843d1ab58
Shougot mollik <shougotmollik@gmail.com>
2023-10-27 02:38:24 +0600

    updated

--- Alpha Batch/Basic java/hello_world.java
+++ Alpha Batch/Basic java/hello_world.java
@@ -1,6 +1,5 @@
 public class hello_world {
-     public static void main(String[]agrs) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
-        
     }
 }
