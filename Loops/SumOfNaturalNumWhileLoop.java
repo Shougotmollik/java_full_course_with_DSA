5d02be289f950c7bbb3f6021293b99b48617e89f
Shougot mollik <shougotmollik@gmail.com>
2023-10-27 02:38:41 +0600

    updated

--- Loops/SumOfNaturalNumWhileLoop.java
+++ Loops/SumOfNaturalNumWhileLoop.java
@@ -7,12 +7,12 @@ public class SumOfNaturalNumWhileLoop {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter the value for n : ");
-            int n=sc.nextInt();
-            int sum=0;
-            int i=1;
-            while (i<=n) {
-                sum=sum+i;
-                System.out.println("The sum is "+sum);
+            int n = sc.nextInt();
+            int sum = 0;
+            int i = 1;
+            while (i <= n) {
+                sum = sum + i;
+                System.out.println("The sum is " + sum);
                 i++;
             }
         }
