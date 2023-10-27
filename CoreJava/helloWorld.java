eaf55c22c566d09bc27b7f0a95f686e8c2f916d7
Shougot mollik <shougotmollik@gmail.com>
2023-10-27 21:57:40 +0600

    updated

---
+++ CoreJava/helloWorld.java
@@ -0,0 +1,8 @@
+package CoreJava;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        System.out.println("Shougot Mollik");
+    }
+}
