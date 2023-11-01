e22122b76ebe1630734b403d753e798af590659e
Shougot mollik <shougotmollik@gmail.com>
2023-11-01 15:33:33 +0600

    updated

--- CoreJava/helloWorld.java
+++ CoreJava/helloWorld.java
@@ -1,8 +1,13 @@
 package CoreJava;
 
+import java.util.Scanner;
+
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello World");
         System.out.println("Shougot Mollik");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println(name);
     }
 }
---
+++ CoreJava/javaInput.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class javaInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
