b0a02ae8db09e9246eb5fd9e36779ef51c379fc6
Shougot mollik <shougotmollik@gmail.com>
2023-11-01 15:33:10 +0600

    updated

---
+++ CoreJava/additonOfThree.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+public class additonOfThree {
+    public static void main(String[] args) {
+        int num1=4;
+        int num2=6;
+        int num3=8;
+        int sum=num1+num2+num3;
+        System.out.print("Sum of the number is : "+sum);
+
+    }
+}
