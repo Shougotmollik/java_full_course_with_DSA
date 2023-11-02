d1c037ee2144c225f55f29ab9c138892a009dc51
Shougot mollik <shougotmollik@gmail.com>
2023-11-02 19:40:03 +0600

    updated

--- CoreJava/javaInput.java
+++ CoreJava/javaInput.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class javaInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a);
+
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+
+        int sum=num1+num2+num3;
+
+        System.out.println("The sum is : "+sum);
     }
 }
