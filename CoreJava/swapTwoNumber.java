70c608cf2bda6fb18d18c7490294614eda3e96db
Shougot mollik <shougotmollik@gmail.com>
2023-11-07 00:09:21 +0600

    swapTwoNumber

---
+++ CoreJava/swapTwoNumber.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class swapTwoNumber {
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+
+
+    }
+}
