7db8dda794bf89487475bcf41f4c567c80eb8218
Shougot mollik <shougotmollik@gmail.com>
2023-11-06 23:48:46 +0600

    updated

---
+++ CoreJava/print1To10While.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class print1To10While {
+    public static void main(String[] args) {
+        int number=1;
+        while (number<=10){
+            System.out.println(number);
+            number++;
+    }
+}
+}
