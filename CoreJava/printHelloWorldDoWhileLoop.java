946dbcd8235795fa785abfd470945f6202ad3f57
Shougot mollik <shougotmollik@gmail.com>
2023-11-08 15:03:46 +0600

    printHelloWorldDoWhileLoop

---
+++ CoreJava/printHelloWorldDoWhileLoop.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class printHelloWorldDoWhileLoop {
+    public static void main(String[] args) {
+        int counter=1;
+        do {
+            System.out.println("Hello World");
+            counter++;
+        }while (counter<10);
+    }
+}
