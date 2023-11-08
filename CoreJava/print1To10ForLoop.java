1dec7c9197da79c7b9a5b15be09a626216fbb19e
Shougot mollik <shougotmollik@gmail.com>
2023-11-08 15:06:03 +0600

    print1To10ForLoop

---
+++ CoreJava/print1To10ForLoop.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+public class print1To10ForLoop {
+    public static void main(String[] args) {
+        int number=10;
+        for (int i =0;i<=number;i++){
+            System.out.println(i);
+        }
+    }
+}
