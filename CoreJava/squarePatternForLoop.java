c6a43d4b5ee4977c270034a7d48958c393989ee5
Shougot mollik <shougotmollik@gmail.com>
2023-11-08 15:04:52 +0600

    squarePatternForLoop

---
+++ CoreJava/squarePatternForLoop.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+public class squarePatternForLoop {
+    public static void main(String[] args) {
+
+//        For loop example
+        for (int lines=1;lines<=4;lines++){
+            System.out.println("* * * *");
+        }
+
+//        while loop example
+    int line=1;
+    while (line<=4){
+        System.out.println("* * * *");
+        line++;
+    }
+    }
+
+}
